fff6a762c894bc055d94fcea90cbbf671920f57b
Frederik Charles Martin <fcm@pensionpartner.dk>
2023-06-20 13:16:45 +0200

    Ready for test

---
+++ ProjectBConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.FileProviders;
+using ProjectAWeb.Localization;
+
+namespace ProjectBConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tmp = new ManifestEmbeddedFileProvider(typeof(Translations).Assembly);
+
+            var names = typeof(Program).Assembly.GetManifestResourceNames();
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
